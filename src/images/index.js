14e75875ff063e5f5ff81ee92b99dfbde336e8b3
santiill <szebiniso0909@gmail.com>
2022-10-17 20:50:24 +0600

    made banner and second section on about us section

--- src/images/index.js
+++ src/images/index.js
@@ -6,11 +6,18 @@ import iconLangusges from './iconLangusges.svg'
 import iconRussions from './ЯзыкиRusion.svg'
 import iconKyrgyzstan from './Языки (1)Ryrgyz.svg'
 import iconOzbekstan from './Языки (2)Ozbekstan.svg'
+import imgAbout from './imgAbout.svg'
+import imgAbout2 from './imgAbout2.svg'
 
 export {
     ArrowsWhitsLeft,
     iconRussions,
     iconKyrgyzstan,
     iconOzbekstan,
-    iconLangusges, LogoTip, ArrowsWhitsRight, MainImages
+    iconLangusges,
+    LogoTip,
+    ArrowsWhitsRight,
+    MainImages,
+    imgAbout,
+    imgAbout2,
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Navbar from "../components/Navbar";
+import {imgAbout, imgAbout2} from "../images";
+import mainStyles from "./../components/Slider/slider.module.css"
+import aboutStyles from "./../Styles/About.module.scss"
 
 export default function About() {
     return (
-        <div>About</div>
+        <div>
+            <Navbar/>
+            <div className={aboutStyles.banner}>
+                <img className={mainStyles.slideImage} src={imgAbout} alt="/"></img>
+                <h1 className={aboutStyles.mainTitle}>Нет звука приятнее,<br/>
+                    чем шёпот бессменных лидеров
+                </h1>
+            </div>
+            <div className="container">
+                <div style={{height: "540px"}} className="flex_between">
+                    <div className={aboutStyles.text_container}>
+                        <h2 className={aboutStyles.title}>Hac maecenas</h2>
+                        <p className={aboutStyles.text}>Есть над чем задуматься: некоторыео собенности внутренней политики
+                            призваны к ответу. Для современного мира социально-экономическое
+                            развитие выявляет срочную потребность глубокомысленных
+                            рассуждений.Повседневная практика показывает, что высокотех-
+                            нологичная концепция общественного уклада требует определения
+                            и уточнения экономической целесообразности принимаемых решений.
+                            В своём стремлении улучшить пользовательский опыт мы упускаем,
+                            что предприниматели в сети интернет будут преданы социально-
+                            демократической анафеме.
+                        </p>
+                        <p className={aboutStyles.text}>Таким образом, социально-экономическое развитие требует определения и уточнения новых предложений. Значимость этих проблем настолько очевидна, что высокое качество позиционных исследований требует определения и уточнения поставленных обществом задач.</p>
+                    </div>
+                    <img style={{width: "636px"}} src={imgAbout2} alt="/"/>
+                </div>
+            </div>
+
+        </div>
     )
 }
